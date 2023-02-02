87616d4b87bd2f31fcc0008f97c8826b3f5c8898
Grazi Firmino <grazi.firmino.15@gmail.com>
2023-02-02 19:57:00 -0300

    Correção de erros
    O código apresentava alguns erros na execução. Acrescentei um import que estava gerando problema de sintaxe, além disso, havia um problema no looping que foi solucionado com a adição da const fala.

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const rl = readLine.createInterface({
 
 let list = [];
 
+const fala = () => {
 rl.question('Insira uma propriedade CSS ou digite "SAIR": ', (answer) => {
   if (answer.toUpperCase() === 'SAIR') {
     rl.close();
   } else {
     list.push(answer);
-    rl.prompt();
+   fala ();
   }
 });
+};
+
+fala ();
+
+rl.on('close', () => {
+    console.log(list.sort((a, b) => a.localeCompare(b, undefined, {locale: 'en'})).join('\n'));
+  });
